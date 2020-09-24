5ebd2cc0cde6cafad57211d65d7656bc04da1ed3
juliojose3000 <juliojose3000@gmail.com>
2020-09-24 17:05:00 -0600

    #DatesGym - SecurityTesting: security was implemented successfully in all test classes

--- src/test/java/com/simple/rest/service/controller/ReservationControllerTest.java
+++ src/test/java/com/simple/rest/service/controller/ReservationControllerTest.java
@@ -2,17 +2,21 @@ package com.simple.rest.service.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import com.simple.rest.service.domain.Authentication;
 import com.simple.rest.service.domain.Reservation;
+import com.simple.rest.service.domain.Token;
 import com.simple.rest.service.domain.User;
 import com.simple.rest.service.util.Dates;
 
@@ -22,17 +26,53 @@ class ReservationControllerTest {
 	
 	private String url = "http://localhost:8086/gymcachi/reservation";
 	
-	private HttpEntity<String> request;
+	private static HttpEntity<String> request;
 	
-	private RestTemplate restTemplate = new RestTemplate();
+	private static RestTemplate restTemplate = new RestTemplate();
+	
+	private static String username = "juliojose3000";
+	
+	private static String password = "123";
+	
+	private static String token;
+	
+	private static HttpHeaders headers;
+	
+	@BeforeAll
+	public static void init() {
+		
+		token = getToken();
+		
+		headers = new HttpHeaders();
+		
+		headers.add("Authorization", token);
+		
+		request = new HttpEntity<String>(headers);
+		
+	}
+	
+	public static String getToken() {
+		
+		Authentication auth = new Authentication(username, password);
+		
+		Token token = 
+				
+			restTemplate.postForObject("http://localhost:8086/gymcachi/authenticate", auth, Token.class);
+		
+		System.out.println(token.getToken());
+		
+		return token.toString();
+		
+	}
 
 	@Test
 	void make() {
 		
-		boolean wasSuccessfulProcess = 
-				this.restTemplate.postForObject(this.url+"/make",getReservation(), Boolean.class);
-		
-		assertTrue(wasSuccessfulProcess == true);
+		HttpEntity<Reservation> request = new HttpEntity<Reservation>(getReservation(), headers);
+		ResponseEntity<Boolean> response = restTemplate.exchange(this.url+"/make", HttpMethod.POST, request, Boolean.class);
+		assertTrue(response.getStatusCode() == HttpStatus.OK);
+		boolean wasSuccessful = response.getBody();
+		assertTrue(wasSuccessful);
 		
 	}
 	
@@ -40,8 +80,8 @@ class ReservationControllerTest {
 		
 		Reservation reservation = new Reservation();
 		
-		User user = new User(2, "Julio","Segura","8734-9630","juliojose3000@gmail.com","","", false);
-		String date = "2020-08-17";
+		User user = new User(1, "Julio","Segura","8734-9630","juliojose3000@gmail.com","","", false);
+		String date = "2020-09-20";
 		String startHour = "9:00:00";
 		
 		reservation.setUser(user);
--- src/test/java/com/simple/rest/service/controller/UserControllerTest.java
+++ src/test/java/com/simple/rest/service/controller/UserControllerTest.java
@@ -2,13 +2,21 @@ package com.simple.rest.service.controller;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import com.simple.rest.service.domain.Authentication;
+import com.simple.rest.service.domain.Reservation;
+import com.simple.rest.service.domain.Token;
 import com.simple.rest.service.domain.User;
 
 @SpringBootTest
@@ -17,23 +25,59 @@ class UserControllerTest {
 	
 	private String url = "http://localhost:8086/gymcachi/user";
 	
-	private HttpEntity<String> request;
+	private static HttpEntity<String> request;
 	
-	private RestTemplate restTemplate = new RestTemplate();
+	private static RestTemplate restTemplate = new RestTemplate();
+	
+	private static String username = "juliojose3000";
+	
+	private static String password = "123";
+	
+	private static String token;
+	
+	private static HttpHeaders headers;
+	
+	@BeforeAll
+	public static void init() {
+		
+		token = getToken();
+		
+		headers = new HttpHeaders();
+		
+		headers.add("Authorization", token);
+		
+		request = new HttpEntity<String>(headers);
+		
+	}
+	
+	public static String getToken() {
+		
+		Authentication auth = new Authentication(username, password);
+		
+		Token token = 
+				
+			restTemplate.postForObject("http://localhost:8086/gymcachi/authenticate", auth, Token.class);
+		
+		System.out.println(token.getToken());
+		
+		return token.toString();
+		
+	}
 	
 	@Test
-	void make() {
+	void register() {
 		
-		boolean wasSuccessfulProcess = 
-				this.restTemplate.postForObject(this.url+"/create", getUser(), Boolean.class);
-		
-		assertTrue(wasSuccessfulProcess == true);
+		HttpEntity<User> request = new HttpEntity<User>(getUser(), headers);
+		ResponseEntity<Boolean> response = restTemplate.exchange(this.url+"/create", HttpMethod.POST, request, Boolean.class);
+		assertTrue(response.getStatusCode() == HttpStatus.OK);
+		boolean wasSuccessful = response.getBody();
+		assertTrue(wasSuccessful);
 		
 	}
 	
 	public User getUser() {
 		
-		return new User(2, "Julio","Segura","8734-9630","juliojose3000@gmail.com","","", false);
+		return new User(2, "Carlor=s","Alvarado","87310475","charlie98@gmail.com","","", false);
 		
 	}
 	
