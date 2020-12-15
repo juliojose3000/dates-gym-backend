3a44b242ebb402c53c47fb217370f1204247905f
Julio Segura <jsegurac@tecnosys.net>
2020-12-14 20:16:01 -0600

    #DatesGym -> Develop: manage invalid credentials and Get schedule creation time

--- src/main/java/com/simple/rest/service/resources/ErrorMessages.java
+++ src/main/java/com/simple/rest/service/resources/ErrorMessages.java
@@ -2,6 +2,7 @@ package com.simple.rest.service.resources;
 
 public class ErrorMessages {
 	
+	public static final String BAD_CREDENTIALS = "Bad credentials";
 	public static final String INVALID_CREDENTIALS = "Cannot invoke \"com.simple.rest.service.domain.User.getEmail()\" because \"user\" is null";
 
 }
