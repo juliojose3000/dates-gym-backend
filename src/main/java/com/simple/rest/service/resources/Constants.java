508f6357445d4fb31b977752173abbfe4e5601be
Julio Segura <jsegurac@tecnosys.net>
2021-02-27 10:30:06 -0600

    #DatesGym -> HerokuDeploy: production variable = true

--- src/main/java/com/simple/rest/service/resources/Constants.java
+++ src/main/java/com/simple/rest/service/resources/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static int HOUR_TO_CREATE_A_NEW_SCHEDULE = 17; //5 PM
 	public static String JSEGURAC_PC = "jsegurac-pc";
-	public static boolean PRODUCTION = false; //TODO put in true to production
+	public static boolean PRODUCTION = true; //TODO put in true to production
 	public static int TRUE = 1;
 	public static int FALSE = 0;
 
